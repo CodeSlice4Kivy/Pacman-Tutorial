d4655991271acb809f10f2c1b5e76004203c4d06
CodeSlice4Kivy <96576667+CodeSlice4Kivy@users.noreply.github.com>
2022-06-24 02:09:04 +0200

    Add files via upload

---
+++ pacman p4 smooth turns and player portal/main.py
@@ -0,0 +1,54 @@
+from kivy.app import App
+from kivy.uix.screenmanager import Screen
+from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty
+from kivy.core.window import Window
+from player import *
+from kivy.clock import Clock
+
+Window.size = (1200,400)
+
+class GamePlay(Screen):
+    ps = NumericProperty(77)
+    ww = NumericProperty(1200)
+    wh = NumericProperty(400)
+
+    pacman = Player()
+
+    def __init__(self, **kwargs):
+        super(GamePlay,self).__init__(**kwargs)
+        self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
+        self._keyboard.bind(on_key_down=self._on_keyboard_down)
+
+    def _keyboard_closed(self):
+        self._keyboard.unbind(on_key_down=self._on_keyboard_down)
+        self._keyboard = None
+
+    def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
+        if keycode[1] == 'up':
+            self.pacman.velocity=(0,1)
+        elif keycode[1] == 'down':
+            self.pacman.velocity=(0,-1)
+        elif keycode[1] == 'left':
+            self.pacman.velocity=(-1,0)
+        elif keycode[1] == 'right':
+            self.pacman.velocity=(1,0)
+            print('hello')
+            
+        return True
+
+    def update(self, dt):
+        self.pacman.move()
+
+# We will use walls to create the maze  
+class Wall(Widget):
+    pass
+
+
+class PacmanApp(App):
+    def build(self):
+        game = GamePlay()
+        Clock.schedule_interval(game.update, 1.0 / 60.0)
+        return game
+
+PacmanApp().run()
